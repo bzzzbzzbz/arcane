5021f159ea425863742963fe4ec32d94770528d4
bzzzbzzbz <ugorelova.su@gmail.com>
2026-07-16 01:13:45 +0700

    оказалось что я удалил не ту строчку

--- Content.Server/_Starlight/Revolutionary/RevSupplyRiftSystem.cs
+++ Content.Server/_Starlight/Revolutionary/RevSupplyRiftSystem.cs
@@ -37,7 +37,7 @@ namespace Content.Server._Starlight.Revolutionary;
 /// </summary>
 public sealed partial class RevSupplyRiftSystem : EntitySystem
 {
-    [Dependency] private Chat.Managers.IChatManager _chatManager = default!;
+    [Dependency] private IChatManager _chatManager = default!;
     [Dependency] private NavMapSystem _navMap = default!;
     [Dependency] private StoreSystem _store = default!;
     [Dependency] private IGameTiming _timing = default!;
