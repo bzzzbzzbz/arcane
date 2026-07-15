484e4b963e35a0d63ce2a0adfb61c85eae30f9ac
bzzzbzzbz <ugorelova.su@gmail.com>
2026-07-15 23:57:01 +0700

    удалил повтор

--- Content.Server/_Starlight/Revolutionary/RevSupplyRiftSystem.cs
+++ Content.Server/_Starlight/Revolutionary/RevSupplyRiftSystem.cs
@@ -37,7 +37,6 @@ namespace Content.Server._Starlight.Revolutionary;
 /// </summary>
 public sealed partial class RevSupplyRiftSystem : EntitySystem
 {
-    [Dependency] private IChatManager _chatManager = default!;
     [Dependency] private Chat.Managers.IChatManager _chatManager = default!;
     [Dependency] private NavMapSystem _navMap = default!;
     [Dependency] private StoreSystem _store = default!;
